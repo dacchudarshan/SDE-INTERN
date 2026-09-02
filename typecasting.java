23b88ab2a4ce1743528d9fd4b442fc1048e0812f
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-02 21:29:19 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,11 @@
+// put smaller data into larger container 
+public class typecasting 
+{
+   public static void main(String[]args)
+   {
+    int num = 100;
+    byte result = (byte)num;
+
+    System.out.println("result :"+ result);
+   } 
+}
