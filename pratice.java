db6237ba1e3bbacd59fba2092f6ab75bfe84f074
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-03 23:38:19 +0530

    Refactor pratice.java to remove commented code

--- pratice.java
+++ pratice.java
@@ -1,160 +1,163 @@
+class pr
+{
+    static byte b;
+    static short c;
+    static double w;
+    static float i;
+    static long f;
+    static int q;
+    static char j;
+    static boolean flag;
+    static int[] arr;
 
-// // // // {
-// // // //     static byte b;
-// // // //     static short c;
-// // // //     static double w;
-// // // //     static float i;
-// // // //     static long f;
-// // // //     static int q;
-// // // //     static char j;
-// // // //     static boolean flag;
-// // // //     static int[] arr;
-
-// // // //     public static void Main(String[] args) 
-// // // //     {
-// // // //         SysteM.out.println("byte: "+ b);
-// // // //         SysteM.out.println("short: "+ c);
-// // // //         SysteM.out.println("double: " + w);
-// // // //         SysteM.out.println("float: "+ i);
-// // // //         SysteM.out.println("long: "+ f);
-// // // //         SysteM.out.println("int: "+ q);
-// // // //         SysteM.out.println("char: "+ (int)j);
-// // // //         SysteM.out.println("boolean: "+ flag);
-// // // //         SysteM.out.println("int: "+ arr);
+    public static void Main(String[] args) 
+    {
+        SysteM.out.println("byte: "+ b);
+        SysteM.out.println("short: "+ c);
+        SysteM.out.println("double: " + w);
+        SysteM.out.println("float: "+ i);
+        SysteM.out.println("long: "+ f);
+        SysteM.out.println("int: "+ q);
+        SysteM.out.println("char: "+ (int)j);
+        SysteM.out.println("boolean: "+ flag);
+        SysteM.out.println("int: "+ arr);
         
-// // // //     }
+    }
 
 
-// // // // }
+}
 
-// // // class pratice
-// // // {
-// // //     public static void Main(String[] args) 
-// // //     {
-// // //         //integer data type 
-// // //         byte bytevalue = 10;
-// // //         short shortvalue = 120;
-// // //         int intvalue = 20000000;
-// // //         long longvalue = 90000000000000l;
+class pratice
+{
+    public static void Main(String[] args) 
+    {
+        //integer data type 
+        byte bytevalue = 10;
+        short shortvalue = 120;
+        int intvalue = 20000000;
+        long longvalue = 90000000000000l;
 
-// // //         //float values
-// // //         float floatvalues = 3.14f;
-// // //         double doublevalues = 3.14285876987982D;
+        //float values
+        float floatvalues = 3.14f;
+        double doublevalues = 3.14285876987982D;
         
-// // //         char charvalue = 'A';
-// // //         boolean booleanvalue = true ;
-// // //         String stringvalues = "darshan";
+        char charvalue = 'A';
+        boolean booleanvalue = true ;
+        String stringvalues = "darshan";
 
-// // //         SysteM.err.println("byte value: "+ bytevalue);
-// // //         SysteM.err.println("byte value: "+shortvalue);
-// // //         SysteM.err.println("byte value: "+ longvalue);  
-// // //         SysteM.err.println("byte value: "+ doublevalues);
-// // //     }
-// // // }
-
-
-// // class pratice
-// // {
-// //     public static void Main(String[] args)
-// //     {
-// //         float M = 1.0f;
-// //         SysteM.err.println("Range: "+Integer.MAX_VALUE);
-// //         SysteM.err.println("Range: "+Integer.MIN_VALUE);
-// //         SysteM.err.println("Range: "+Integer.SIZE);
-// //     }
-// // }
+        SysteM.err.println("byte value: "+ bytevalue);
+        SysteM.err.println("byte value: "+shortvalue);
+        SysteM.err.println("byte value: "+ longvalue);  
+        SysteM.err.println("byte value: "+ doublevalues);
+    }
+}
 
 
-// class pratice 
-// {
-//     public static void main(String[] args) 
-//     {
-//         // byte c = 126 ;//126
-//         // SysteM.out.println(b);//126
-//         // b++;
-//         // SysteM.out.println(b);//127
-//         // b++;
-//         // SysteM.out.println(b);//-128
+class pratice
+{
+    public static void Main(String[] args)
+    {
+        float M = 1.0f;
+        SysteM.err.println("Range: "+Integer.MAX_VALUE);
+        SysteM.err.println("Range: "+Integer.MIN_VALUE);
+        SysteM.err.println("Range: "+Integer.SIZE);
+    }
+}
+
+
+class pratice 
+{
+    public static void main(String[] args) 
+    {
+        byte c = 126 ;//126
+        SysteM.out.println(b);//126
+        b++;
+        SysteM.out.println(b);//127
+        b++;
+        SysteM.out.println(b);//-128
         
 
-//         byte darshu = Byte.MIN_VALUE ;
-//         System.out.println(darshu);
-//         darshu--;
-//         System.out.println(darshu);
-//         darshu--;
-//         System.out.println(darshu);
-//     }
-// }
+        byte darshu = Byte.MIN_VALUE ;
+        System.out.println(darshu);
+        darshu--;
+        System.out.println(darshu);
+        darshu--;
+        System.out.println(darshu);
+    }
+}
 
 
-// class pratice 
-// {
-//         static char ch = '5';
+class pratice 
+{
+        static char ch = '5';
 
-//         public static void main(String[] args) {
+        public static void main(String[] args) {
             
-//             System.out.println(ch);
+            System.out.println(ch);
 
-//         }
-
-
-// }
-
-// class Practice {
-//     static char ch = '5';
-
-//     public static void main(String[] args) {
-
-//         System.out.println(ch);
-
-//         if (((Object) ch) instanceof Character) {
-//             System.out.println("ch is a Character (char datatype)");
-//         }
-//     }
-// }
+        }
 
 
-// swap there number 
+}
 
-// class pratice
-// {
-//     public static void main(String[] args) 
-//     {
-//         System.err.println("before swappping: ");
-//         byte num1 = 10;
-//         byte num2 = 20;
-//         byte num3 = 30;
+class Practice 
+{
+    static char ch = '5';
 
-//         System.err.println("num1: "+num1);
-//         System.err.println("num1: "+num2);
-//         System.err.println("num1: "+num3);
+    public static void main(String[] args) 
+        {
 
-//         System.err.println("ofter swapping: ");
+        System.out.println(ch);
 
-//         int temp = num1;
-
-//         num1 = num2;
-//         num2 = num3;
-//         num3 = (byte)temp;
-
-//         System.err.println("num1: "+num1);
-//         System.err.println("num1: "+num2);
-//         System.err.println("num1: "+num3);
+        if (((Object) ch) instanceof Character) 
+        {
+            System.out.println("ch is a Character (char datatype)");
+        }
+    }
+}
 
 
-//     }
-// }
+swap there number 
 
-// class a
-// {
-//     public static void main(String[] args) 
-//     {
+class pratice
+{
+    public static void main(String[] args) 
+    {
+        System.err.println("before swappping: ");
+        byte num1 = 10;
+        byte num2 = 20;
+        byte num3 = 30;
 
-//         int x = 15, y = 25, z;
-//         z = x+y;
+        System.err.println("num1: "+num1);
+        System.err.println("num1: "+num2);
+        System.err.println("num1: "+num3);
 
-//         String str = new String("sum "+z);
-//         System.err.println(str);
-//     }
-// }
+        System.err.println("ofter swapping: ");
+
+        int temp = num1;
+
+        num1 = num2;
+        num2 = num3;
+        num3 = (byte)temp;
+
+        System.err.println("num1: "+num1);
+        System.err.println("num1: "+num2);
+        System.err.println("num1: "+num3);
+
+
+    }
+}
+
+class a
+{
+    public static void main(String[] args) 
+    {
+
+        int x = 15, y = 25, z;
+        z = x+y;
+
+        String str = new String("sum "+z);
+        System.err.println(str);
+    }
+}
 
