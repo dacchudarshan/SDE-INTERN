dae41ee823d5b1acaf4e3bbffe1ae45b7083a3cf
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-03 23:35:48 +0530

    Update revission.java

--- revission.java
+++ revission.java
@@ -1,82 +1,83 @@
 
-// // public class revission
-// // {
-// //     public static void main(String[] args) 
-// //     {
-// //         System.err.println("addition of two numbers man");
-// //         Scanner add = new Scanner(System.in);
-// //         int a, b, total;
+public class revission
+{
+    public static void main(String[] args) 
+    {
+        System.err.println("addition of two numbers man");
+        Scanner add = new Scanner(System.in);
+        int a, b, total;
 
-// //         System.err.println("Enter values A: ");
-// //         a = add.nextInt();
+        System.err.println("Enter values A: ");
+        a = add.nextInt();
         
-// //         System.err.println("Enter values B: ");
-// //         b = add.nextInt();
-// //         total = a + b;
-// //         System.err.println("total values is: "+total);
+        System.err.println("Enter values B: ");
+        b = add.nextInt();
+        total = a + b;
+        System.err.println("total values is: "+total);
 
-// //     }
-// // }
+    }
+}
 
 
 
-// class dec
-// {
-//     public static void main(String[] args) 
-//     {
-//         int count = 10;
-//         System.out.println(count--);
-//         System.out.println(count);
-//         System.out.println(count--);
-//         System.out.println(count);
-//         System.out.println(count--);
-//         System.out.println(count);
-//         System.out.println(count--);
-//           System.out.println(count);
-//             System.out.println(count--);
-//               System.out.println(count); System.out.println(--count);System.out.println(--count);System.out.println(--count);
+class dec
+{
+    public static void main(String[] args) 
+    {
+        int count = 10;
+        System.out.println(count--);
+        System.out.println(count);
+        System.out.println(count--);
+        System.out.println(count);
+        System.out.println(count--);
+        System.out.println(count);
+        System.out.println(count--);
+        System.out.println(count);
+        System.out.println(count--);
+        System.out.println(count); System.out.println(--count);System.out.println(--count);System.out.println(--count);
          
-//             System.out.println();
-//         int a = 10;
-//         System.out.println(--a);
-//             System.out.println(--a);
-//             System.out.println(a);
-//             System.out.println(--a);
-//             System.out.println(--a);
-//             System.out.println(--a);
-//             System.out.println(--a);
-//             System.out.println(a);
-//             System.out.println(--a);
-//             System.out.println(--a);
-//             System.out.println(--a);
-//             System.out.println(--a);
-//             System.out.println(--a);System.out.println(--a);System.out.println(--a);System.out.println(--a);System.out.println(--a);
+        System.out.println();
+        
+            int a = 10;
+            System.out.println(--a);
+            System.out.println(--a);
+            System.out.println(a);
+            System.out.println(--a);
+            System.out.println(--a);
+            System.out.println(--a);
+            System.out.println(--a);
+            System.out.println(a);
+            System.out.println(--a);
+            System.out.println(--a);
+            System.out.println(--a);
+            System.out.println(--a);
+            System.out.println(--a);System.out.println(--a);System.out.println(--a);System.out.println(--a);System.out.println(--a);
 
-//         System.out.println();
-//         int count1 = 12;
-//         int count2 = count1++;
+        System.out.println();
+        int count1 = 12;
+        int count2 = count1++;
 
-//         System.out.println("c2: "+count2++);
-//         System.out.println(count1++);
+        System.out.println("c2: "+count2++);
+        System.out.println(count1++);
 
-//       System.out.println(count1++);
-//        System.out.println(count1++);
-//         System.out.println(count1++);
+      System.out.println(count1++);
+       System.out.println(count1++);
+        System.out.println(count1++);
 
-//         System.out.println();
+        System.out.println();
+                
+           System.out.println("c2: "+count2++);
+           System.out.println(count2++);
+            System.out.println(count2++);
+            System.out.println();
+            System.out.println(count2--);
+            System.out.println(count2--);
 
-//          System.out.println("c2: "+count2++);
-//           System.out.println(count2++);
-//            System.out.println(count2++);
-//            System.out.println();
-//             System.out.println(count2--);
-//              System.out.println(count2--);
 
 
-
-//     }     
+    }     
    
-// }
+}
 class operators
 {
     public static void main(String[] args)
