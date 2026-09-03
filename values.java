4fa690ec80cbb56ef7fa2a9d70d2b7b28f2e45ff
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-03 23:32:57 +0530

    Update values.java

--- values.java
+++ values.java
@@ -2,18 +2,18 @@ public class values
 {
     public static void main(String[] args) 
     {
-        // int num1 = 10;// values
-        // int num2 = 10;// values 
+        int num1 = 10;// values
+        int num2 = 10;// values 
 
-        // //if (num1 == num2) // works for primitive data types
-        // // if (num.equals(num2)) object invokting 
-        // {
-        //     System.out.println("Both numbers are equal.");
-        // } 
-        // else 
-        // {
-        //     System.out.println("Numbers are not equal.");
-        // }
+        //if (num1 == num2) // works for primitive data types
+        // if (num.equals(num2)) object invokting 
+        {
+            System.out.println("Both numbers are equal.");
+        } 
+        else 
+        {
+            System.out.println("Numbers are not equal.");
+        }
     
 
         //int day = "saturday"; incompatible types: String cannot be converted to int
