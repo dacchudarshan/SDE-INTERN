0217d132c3c9c9c32b65b495c4dc7dfa1d638664
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-07 21:07:40 +0530

    Refactor SwapNumbers for clarity and functionality

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,91 +1,91 @@
-// // extra memory using 3 number
-// public class SwapNumbers 
-// {
-//     public static void main(String[] args) 
-//     {
-//         System.err.println("Before swalppping : ");
-//         int a = 10;
-//         int b = 20;
-//         int c = 30;
+// extra memory using 3 number
+public class SwapNumbers 
+{
+    public static void main(String[] args) 
+    {
+        System.err.println("Before swalppping : ");
+        int a = 10;
+        int b = 20;
+        int c = 30;
 
-//         System.err.println("a: "+a);
-//         System.err.println("b: "+b);
-//         System.err.println("c: "+c);
+        System.err.println("a: "+a);
+        System.err.println("b: "+b);
+        System.err.println("c: "+c);
 
-//         System.out.println("Ofter Swapiing: " );
+        System.out.println("Ofter Swapiing: " );
         
-//         int temp = a;
+        int temp = a;
 
-//         a = b;
-//         b = c;
-//         c = temp;
+        a = b;
+        b = c;
+        c = temp;
 
-//         System.err.println("a: "+a);
-//         System.err.println("b: "+b);
-//         System.err.println("c: "+c);
-//     }
-// }
+        System.err.println("a: "+a);
+        System.err.println("b: "+b);
+        System.err.println("c: "+c);
+    }
+}
 
-// without extra memory 3 number 
-// public class SwapNumbers 
-// {
-//     public static void main(String[] args) 
-//     {
-//         System.err.println("Before swalppping : ");
+without extra memory 3 number 
+public class SwapNumbers 
+{
+    public static void main(String[] args) 
+    {
+        System.err.println("Before swalppping : ");
 
-//         int a = 10;
-//         int b = 20;
-//         int c = 30;
+        int a = 10;
+        int b = 20;
+        int c = 30;
 
-//         System.err.println("a: "+a);
-//         System.err.println("b: "+b);
-//         System.err.println("c: "+c);
+        System.err.println("a: "+a);
+        System.err.println("b: "+b);
+        System.err.println("c: "+c);
 
-//         System.out.println("Ofter Swapiing: " );
+        System.out.println("Ofter Swapiing: " );
         
-//         a = a + b + c;
-//         c = a - b - c;
-//         b = a - b - c;
-//         a = a - b - c;
+        a = a + b + c;
+        c = a - b - c;
+        b = a - b - c;
+        a = a - b - c;
 
-//         System.err.println("a: "+a);
-//         System.err.println("b: "+b);
-//         System.err.println("c: "+c);
-//     }
-// }
+        System.err.println("a: "+a);
+        System.err.println("b: "+b);
+        System.err.println("c: "+c);
+    }
+}
 
-// // four number swapping with extra memory
+// four number swapping with extra memory
 
-// public class SwapNumbers 
-// {
-//     public static void main(String[] args) 
-//     {
-//         System.err.println("Before swalppping : ");
-//         int a = 10;
-//         int b = 20;
-//         int c = 30;
-//         int d = 40;
+public class SwapNumbers 
+{
+    public static void main(String[] args) 
+    {
+        System.err.println("Before swalppping : ");
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
 
-//         System.err.println("a: "+a);
-//         System.err.println("b: "+b);
-//         System.err.println("c: "+c);
-//         System.err.println("c: "+d);
+        System.err.println("a: "+a);
+        System.err.println("b: "+b);
+        System.err.println("c: "+c);
+        System.err.println("c: "+d);
 
-//         System.out.println("Ofter Swapiing: " );
+        System.out.println("Ofter Swapiing: " );
         
-//         int temp = a;
+        int temp = a;
 
-//         a = b;
-//         b = c;
-//         c = d;
-//         d = temp;
+        a = b;
+        b = c;
+        c = d;
+        d = temp;
 
-//         System.err.println("a: "+a);
-//         System.err.println("b: "+b);
-//         System.err.println("c: "+c);
-//         System.err.println("c: "+d);
-//     }
-// }
+        System.err.println("a: "+a);
+        System.err.println("b: "+b);
+        System.err.println("c: "+c);
+        System.err.println("c: "+d);
+    }
+}
 
 // Without extra memory using 4 number 
 
