ba8813d3954588e31a0a0bbd6a024c7b9963698f
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-07 21:07:24 +0530

    Update ThreeNumber.java

--- ThreeNumber.java
+++ ThreeNumber.java
@@ -1,47 +1,47 @@
-// public class FourNumber 
-// {
-//     public static void main(String[] args) 
-//     {
+public class FourNumber 
+{
+    public static void main(String[] args) 
+    {
 
-//         // Four Number Swap 
+        // Four Number Swap 
 
-//         int Value1 = 10;
-//         int Value2 = 20;
-//         int Value3 = 30;
-//         int Value4 = 40;
+        int Value1 = 10;
+        int Value2 = 20;
+        int Value3 = 30;
+        int Value4 = 40;
 
-//         System.out.println();
-//         System.out.println("Value1 before Swapping : " + Value1);
-//         System.out.println();
-//         System.out.println("Value2 before Swapping :  " + Value2);
-//         System.out.println();
-//         System.out.println("Value3 before Swapping : " + Value3);
-//         System.out.println();
-//         System.out.println("Value4 before Swapping : " + Value4);
+        System.out.println();
+        System.out.println("Value1 before Swapping : " + Value1);
+        System.out.println();
+        System.out.println("Value2 before Swapping :  " + Value2);
+        System.out.println();
+        System.out.println("Value3 before Swapping : " + Value3);
+        System.out.println();
+        System.out.println("Value4 before Swapping : " + Value4);
 
-//         // Logic - Four Number
-//         Value1 = Value4 - Value2; // 20 
-//         Value2 = Value3 + Value2; // 50
-//         Value3 = Value2 - Value4; // 10
-//         Value4 = Value4 - Value3; // 30
-//         Value2 = Value2 - Value3; // 40
+        // Logic - Four Number
+        Value1 = Value4 - Value2; // 20 
+        Value2 = Value3 + Value2; // 50
+        Value3 = Value2 - Value4; // 10
+        Value4 = Value4 - Value3; // 30
+        Value2 = Value2 - Value3; // 40
 
-//         System.out.println();
+        System.out.println();
 
-//         System.out.println("____________After Swapping______________");
+        System.out.println("____________After Swapping______________");
 
-//         System.out.println();
-//         System.out.println("Value1 after Swapping : " + Value1);
-//         System.out.println();
-//         System.out.println("Value2 after Swapping :  " + Value2);
-//         System.out.println();
-//         System.out.println("Value3 after Swapping : " + Value3);
-//         System.out.println();
-//         System.out.println("Value4 after Swapping : " + Value4);
+        System.out.println();
+        System.out.println("Value1 after Swapping : " + Value1);
+        System.out.println();
+        System.out.println("Value2 after Swapping :  " + Value2);
+        System.out.println();
+        System.out.println("Value3 after Swapping : " + Value3);
+        System.out.println();
+        System.out.println("Value4 after Swapping : " + Value4);
 
-//     }
+    }
 
-// }
+}
 
 
 public class ThreeNumber 
