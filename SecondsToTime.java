fe09a2ae1bc292cc3be1a81b9f1feb0ee31b05a8
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-08 09:12:00 +0530

    Update SecondsToTime.java

--- SecondsToTime.java
+++ SecondsToTime.java
@@ -16,25 +16,25 @@
 //     }
 // }
 
-// //sec to time include days 
-// class SecondsTodays
-// {
-//        public static void main(String[] args) 
-//     {
+//sec to time include days 
+class SecondsTodays
+{
+       public static void main(String[] args) 
+    {
 
-//         int totalSeconds = 90061;
+        int totalSeconds = 90061;
 
-//         int days = totalSeconds / 86400;
-//         int hours = (totalSeconds % 86400) / 3600;
-//         int minutes = (totalSeconds % 3600) / 60;
-//         int seconds = totalSeconds % 60;
+        int days = totalSeconds / 86400;
+        int hours = (totalSeconds % 86400) / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
 
-//         System.out.println(days + " days");
-//         System.out.println(hours + " hours");
-//         System.out.println(minutes + " minutes");
-//         System.out.println(seconds + " seconds");
-//     }
-// }
+        System.out.println(days + " days");
+        System.out.println(hours + " hours");
+        System.out.println(minutes + " minutes");
+        System.out.println(seconds + " seconds");
+    }
+}
 
 // reverse 3 digit number
 class reverasnumber
