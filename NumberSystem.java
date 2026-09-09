5154cf7067d89b9266585a4416a55b93a21ed744
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-09 09:13:01 +0530

    Uncomment NumberSystem class and main method

--- NumberSystem.java
+++ NumberSystem.java
@@ -1,23 +1,23 @@
-// public class NumberSystem
-// {    
-//     public static void main(String[] args)
-//     {
-//         String s1 = "10";   //binary                                                 
-//         String s2 = "123";  //octal
-//         String s3 = "329";  //hexadecimal
-//         String s4 = "14";   //decimal
+public class NumberSystem
+{    
+    public static void main(String[] args)
+    {
+        String s1 = "10";   //binary                                                 
+        String s2 = "123";  //octal
+        String s3 = "329";  //hexadecimal
+        String s4 = "14";   //decimal
 
-//         int binary      = Integer.parseInt(s1, 2);  //output: 2^1*1 + 2^0*0 = 2
-//         int octal       = Integer.parseInt(s2, 8);  //output: 8^2*1 + 8^1*2 + 8^0*3 = 64 + 16 + 3 = 83
-//         int hexadecimal = Integer.parseInt(s3, 16); //output: 16^2*3 + 16^1*2 + 16^0*9 = 768 + 32 + 9 = 809
-//         int decimal     = Integer.parseInt(s4, 10); //output: 10^1*1 + 10^0*4 = 14
+        int binary      = Integer.parseInt(s1, 2);  //output: 2^1*1 + 2^0*0 = 2
+        int octal       = Integer.parseInt(s2, 8);  //output: 8^2*1 + 8^1*2 + 8^0*3 = 64 + 16 + 3 = 83
+        int hexadecimal = Integer.parseInt(s3, 16); //output: 16^2*3 + 16^1*2 + 16^0*9 = 768 + 32 + 9 = 809
+        int decimal     = Integer.parseInt(s4, 10); //output: 10^1*1 + 10^0*4 = 14
 
-//         System.out.println("Binary = " + binary);
-//         System.out.println("Octal = " + octal);
-//         System.out.println("Hexadecimal = " + hexadecimal);
-//         System.out.println("Decimal = " + decimal);
-//     }
-// }
+        System.out.println("Binary = " + binary);
+        System.out.println("Octal = " + octal);
+        System.out.println("Hexadecimal = " + hexadecimal);
+        System.out.println("Decimal = " + decimal);
+    }
+}
 
 import java.util.Scanner;
 class NumberSystems
