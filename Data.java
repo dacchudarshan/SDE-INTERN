6421968bf79452bd84ce1d633dd2815786a26440
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-09 09:14:44 +0530

    Refactor 'days' class and clean up comments
    Refactored the 'days' class and removed commented-out code.

--- Data.java
+++ Data.java
@@ -1,4 +1,4 @@
-/*public class days 
+public class days 
 {
     public static void main(String[] args) {
         {
@@ -11,7 +11,7 @@
             System.err.println("true");
         }
     }
-}*/
+}
 // why variable even though we can print value directly 
 //because for feature use if i want you change data then i cant change all the 100 entires so we need variable then below is the code demonsatrate 
 class days
