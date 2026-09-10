6877fe598669e2791c6a90d693589483a4b7679c
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-10 09:43:18 +0530

    Uncomment pattern counting logic in patrn.java
    Uncomment code to count patterns and print with zero padding.

--- patrn.java
+++ patrn.java
@@ -13,22 +13,22 @@ public class patrn
         // }
 
 
-    // // 2) count patterns
-    //     int count = 0;
-    //     for (int i = 1; i <= 5; i++)
-    //     {
-    //         for (int j = 1; j <= 5; j++)
-    //         {
-    //             ++count;
-    //             // normal print
-    //             // System.out.print(count + " ");
+    // 2) count patterns
+        int count = 0;
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                ++count;
+                // normal print
+                // System.out.print(count + " ");
 
-    //             // formated zero padding
-    //             System.out.printf("%02d ", count);
+                // formated zero padding
+                System.out.printf("%02d ", count);
                 
-    //         }
-    //         System.out.println(" ");
-    //     }
+            }
+            System.out.println(" ");
+        }
 
     // 3) 5 cross 5 box print 1 coloum all and second coloum all and so on
         for (int i = 1; i <= 5; i++)
