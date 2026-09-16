392be75cf3ae5ba426e3af011c7703159cc6cfe2
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-16 14:28:00 +0530

    Update douts.java

--- douts.java
+++ douts.java
@@ -2,22 +2,22 @@ public class douts
 {
     public static void main(String[] args) 
     {
-        // for(int index = 2001; index <=200; index++)
-        // {
-        //     System.out.println(index +": appa loves amma");
-        // }
+        for(int index = 2001; index <=200; index++)
+        {
+            System.out.println(index +": appa loves amma");
+        }
     
-        // multiplication in 5
-        // for(int index = 1; index <= 10; index+=2)
+        //multiplication in 5
+        for(int index = 1; index <= 10; index+=2)
           
-        // {
-        //     System.err.println(index*2);
-        // }
+        {
+            System.err.println(index*2);
+        }
         
-        // for(int index = 64/2; index >= 1; index/=12)
+        for(int index = 64/2; index >= 1; index/=12)
           
-        // {
-        //     System.err.println(index);
+        {
+            System.err.println(index);
 
         
 
