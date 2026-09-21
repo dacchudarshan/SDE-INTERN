59d651f2a256e146c5371d0bd02b97ec3cab5927
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-21 20:23:52 +0530

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,6 +1,7 @@
 public class days 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         {
             System.out.println(25);
             System.out.println(3.14);
