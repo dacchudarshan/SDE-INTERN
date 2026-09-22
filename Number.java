7f8dfcc67471863ee2ca85071a4070a131e5c812
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-22 10:16:09 +0530

    Update Number.java

--- Number.java
+++ Number.java
@@ -5,7 +5,7 @@ public class Number
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
-
+ 
         System.out.println("Select Number System:");
         System.out.println("1. Binary"); 
         System.out.println("2. Octal");
