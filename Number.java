b4af56c81cfa263834546bbcb9aa17c7d2ed16d0
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-22 10:16:27 +0530

    Fix formatting of Scanner initialization

--- Number.java
+++ Number.java
@@ -4,7 +4,7 @@ public class Number
 {
     public static void main(String[] args) 
     {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
  
         System.out.println("Select Number System:");
         System.out.println("1. Binary"); 
