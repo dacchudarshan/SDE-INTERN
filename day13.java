c31a671553a3c881e1ecb30ec9d958a90e861fca
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-25 09:11:09 +0530

    Fix formatting issue in day13.java

--- day13.java
+++ day13.java
@@ -15,7 +15,7 @@ public class day13
 
         System.err.println("Scenario 2: both using new ");
         String s3 = new String("java");
-        String s4 = new String("java");
+        String s4 = new String("java"); 
 
         System.err.println("S3 == S4: "+ (s3 == s4)); 
         System.err.println("S3.equals(S4): "+ (s3.equals(s4)));
