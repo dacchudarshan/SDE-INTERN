641de9eef85486dc5f812dddc05f1205c2993cb6
DARSHAN K N <236083882+dacchudarshan@users.noreply.github.com>
2026-09-25 09:11:31 +0530

    Fix formatting of string equality check output

--- day13.java
+++ day13.java
@@ -18,7 +18,7 @@ public class day13
         String s4 = new String("java"); 
 
         System.err.println("S3 == S4: "+ (s3 == s4)); 
-        System.err.println("S3.equals(S4): "+ (s3.equals(s4))); 
+        System.err.println("S3.equals(S4): "+ (s3.equals(s4)));  
 
         System.err.println("--------------------------------------------------");
 
